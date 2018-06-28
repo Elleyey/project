a8c5dd2763e03fac646e319c16d108c34dbaf125
Elleyey <ellemijke@hotmail.com>
2018-06-28 11:47:40 +0200

    probeer pages te fixen

--- webpage/javascript/Netherlands.js
+++ webpage/javascript/Netherlands.js
@@ -4,9 +4,9 @@ for final project programmeren.*/
 window.onload = function() {
 
   queue()
-    .defer(d3.json, 'data/Burglary.json')
-    .defer(d3.json, 'data/Netherlands.json')
-    .defer(d3.json, 'data/crimeRatesTotals.json')
+    .defer(d3.json, '../project/webpage/data/Burglary.json')
+    .defer(d3.json, '../project/webpage/data/Netherlands.json')
+    .defer(d3.json, '../project/webpage/data/crimeRatesTotals.json')
     .awaitAll(getData);
 
     /*getData to reformat data, to be able to use it in different graphs. Calls
